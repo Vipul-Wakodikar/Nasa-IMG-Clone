fda0aee7ae47e2a0da02db164b10a0b0f4321026
Vipul-Wakodikar <wakodikar.vipul@gmail.com>
2024-02-12 19:13:19 +0530

    origin-all-test

--- server/index.js
+++ server/index.js
@@ -4,9 +4,17 @@ const cors = require("cors");
 require('dotenv').config();
 
 app.use(cors({
-    origin: ['localhost:5173', 'https://nasa-image.onrender.com/', 'https://65c667d8f5c4b823ab7febc8--elegant-gaufre-f57a38.netlify.app/'],
-    optionsSuccessStatus: 200,
+    origin: ["localhost:5173", "https://nasa-image.onrender.com/", "https://65c667d8f5c4b823ab7febc8--elegant-gaufre-f57a38.netlify.app/"],
+    // optionsSuccessStatus: 200,
+    default: "https://nasa-image.onrender.com/",
 }))
+
+app.all('*', function(req, res, next) {
+  const origin = cors.origin.includes(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Backend is running baby");
