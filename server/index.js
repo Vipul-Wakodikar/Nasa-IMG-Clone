497cb5d4ba57dcc053a93a5c28a51593a766098d
Vipul-Wakodikar <wakodikar.vipul@gmail.com>
2024-02-13 08:53:49 +0530

    setHeaders-origin

--- server/index.js
+++ server/index.js
@@ -3,17 +3,17 @@ const app = express();
 const cors = require("cors");
 require('dotenv').config();
 
-app.use(cors({
-    origin: "*",
-    optionsSuccessStatus: 200,
-}))
+// app.use(cors({
+//     origin: "*",
+//     optionsSuccessStatus: 200,
+// }))
 
-// app.all('*', function(req, res, next) {
-//   const origin = cors.origin.includes(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
-//   res.header("Access-Control-Allow-Origin", origin);
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://nasa-image.onrender.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Backend is running baby");
